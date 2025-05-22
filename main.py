9acbee762bdcba006d679fc7ddc86e3f85666def
akshat227 <akshatkhare364@gmail.com>
2025-05-22 14:40:21 +0530

    akshat

--- main.py
+++ main.py
@@ -4,22 +4,37 @@ import os
 # from cli import template
 
 app = typer.Typer()
-
+slice_app = typer.Typer(help="Slice-related commands")
+app.add_typer(slice_app, name="slice")
 # app.add_typer(template.app,name="template")
 
 @app.command()
 def ping():
     print("COOK CLI TOOL - PING")
 
+@slice_app.command("add")
+def add(add: str):
+    print("new " + add + " code snipped added!")
 
-@app.command("file")
-def file(file_name: str):
-    with open(file_name, "w") as f:
-       print(file_name + " was created!")
-       f.write(''' 
-print ("hello world")
-''')
-            
+@slice_app.command("share")
+def add(share: str):
+    print("shared!")
+
+@slice_app.command("list")
+def add():
+    print("listed")
+
+@slice_app.command("remove")
+def add(remove: str):
+    print("Removed")
+
+@slice_app.command("")
+def add(add: str):
+    print("new " + add + " code snipped added!")
+
+@slice_app.command("add")
+def add(add: str):
+    print("new " + add + " code snipped added!")          
 
 if __name__=="__main__":
     app()
