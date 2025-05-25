881af1761adf80ca36930fc807ee7e87570e42b5
Vishnu Gupta <thenormvg@gmail.com>
2025-05-26 01:20:51 +0530

    added the slice

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,26 +1,71 @@
 import os
+import platformdirs
+import json
+
+from handlers.pyprompt import Terminal
 
 
-def validateArgs (*args) :
 
-  if set(args) == {None}:
-    return False
+pyp = Terminal()
 
-  if None in args:
-    print("FILL ALL THE ARGS")
-    return None
-  else:
-    return True
+def validateArgs(*args):
+
+    if set(args) == {None}:
+        return False
+
+    if None in args:
+        # print("FILL ALL THE ARGS")
+        pyp.error("All arguments are required. Please provide valid inputs.")
+        return None
+    else:
+        return True
 
 
 def getConfigPath(dirPath):
-  return os.path.join(dirPath,"cook.conf.json")
+
+    return os.path.join(dirPath, "cook.conf.json")
+
 
 def checkConfigFile(dirPath):
-  return os.path.isfile(getConfigPath(dirPath))
+
+    return os.path.isfile(getConfigPath(dirPath))
 
 
 def createCookConfigFile(dirPath):
-  file = open(getConfigPath(dirPath),"w")
-  file.close()
-  return
+
+    file = open(getConfigPath(dirPath), "w")
+    file.close()
+
+    config_data = {
+        "name": "",
+        "author": "",
+        "stir": True,
+        "cmd": {
+            "serve": [],
+            "clean": []
+        },
+        "template": {
+            "name": "",
+            "category": "",
+            "date": "",
+            "author": "",
+            "stack": [],
+            "github": ""
+        }
+    }
+
+
+
+
+    with open(getConfigPath(dirPath), "w") as file:
+        json.dump(config_data, file, indent=4)
+
+    return
+
+
+def GetUserDataDir():
+    appname = "cook-cli"
+    appauthor = "TheAlphaOnes"
+    dir_user_dir = platformdirs.user_data_dir(appname, appauthor)
+
+    return dir_user_dir
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import typer
 # from pathlib import Path
+from cli import slice as cook_slice
 from cli import template
 import os
+import warnings
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
 
 
 app = typer.Typer()
 
+
 app.add_typer(template.app,name="mold")
+app.add_typer(cook_slice.app,name="slice")
+
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,7 +23,14 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.command(".")
 def ping():
-    print("COOK CLI TOOL - PING")
+    # print("COOK CLI TOOL - PING")
+    print(r'''
+  _________  ____  __ __
+ / ___/ __ \/ __ \/ //_/
+/ /__/ /_/ / /_/ / ,<
+\___/\____/\____/_/|_|
+
+''')
 
 
 
